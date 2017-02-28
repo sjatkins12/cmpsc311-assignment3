23ab5ec07c9ac285fda1ac17719da47a070f0914
Sam Atkins <samatkins@client-104-38-106-96.mobility.psu.edu>
2017-02-28 14:40:12 -0500

    Update Git Files

--- crud_file_io.c
+++ crud_file_io.c
@@ -114,33 +114,33 @@ int32_t crud_read(int16_t fd, void *buf, int32_t count) {
 	CrudResponse response;
 	CrudRequest request = fd;
 	int i = 0;
-	void *tbuf;
+	void *tbuf = malloc(count);
 	
 	request <<= 4;
 	request += CRUD_READ;
 	request <<= 24;
-	request += CRUD_MAX_OBJECT_SIZE;
+	request += count;
 	request <<= 4;
-	response = crud_bus_request(request, buf);
-	tbuf = buf;
+	response = crud_bus_request(request, tbuf);
+	//tbuf = buf;
 	printf("%lu\n", response);
 	if (response & 0x1)
 		return (-1); 
 	response >>= 4;
 	printf("Val: %lu\n", (response & 0xFFFFFF));
-	while(i < count) {
-		if (tbuf == NULL)
-			break ;
-		i++;
-		tbuf++;
-	}
-	return (i);
-	// if ((response & 0xFFFFFF) < count)
-	// 	return (response & 0xFFFFFF);
-	// else {
-	// 	printf("Boobs\n");
-	// 	return (i);
+	// while(i < count) {
+	// 	if (tbuf == NULL)
+	// 		break ;
+	// 	i++;
+	// 	tbuf++;
 	// }
+	// return (i);
+	if ((response & 0xFFFFFF) < count)
+		return (response & 0xFFFFFF);
+	else {
+		printf("Boobs\n");
+		return (count);
+	}
 	
 
 }
